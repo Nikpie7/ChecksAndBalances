f4255fc34b0afd9cd9ba2aef0261025aefbbb553
Nikpie7 <nikolai.w.c@gmail.com>
2023-10-17 13:26:05 -0400

    Deployment scripts 2

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 # Update package lists
-sudo apt-get update
+sudo yum update
 
 # Install dependencies (example: nginx)
-sudo apt-get install -y nginx
-
-# Stop the service if it's running (example: nginx)
-sudo systemctl stop nginx
+sudo yum install -y nginx
--- scripts/setup_configuration.sh
+++ scripts/setup_configuration.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Example: Move deployed files to the web server directory
-sudo mv /usr/share/nginx/html/
+sudo mv WebApp/* /var/www/html/
 
