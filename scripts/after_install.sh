49a27ff75edda4e88053720df675f8b1254f0f66
Nikpie7 <nikolai.w.c@gmail.com>
2023-10-18 00:19:58 -0400

    New Deployment v9

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo 'run after_install.sh: ' >> /home/ec2-user/ChecksAndBalances/deploy.log
 
-echo 'cd /home/ec2-user/nodejs-server-cicd' >> /home/ec2-user/ChecksAndBalances/deploy.log
-cd /home/ec2-user/ChecksAndBalances >> /home/ec2-user/ChecksAndBalances/deploy.log
+echo 'cd /home/ec2-user/ChecksAndBalances/WebApp/frontend' >> /home/ec2-user/ChecksAndBalances/deploy.log
+cd /home/ec2-user/ChecksAndBalances/WebApp/frontend >> /home/ec2-user/ChecksAndBalances/deploy.log
 
 echo 'npm install' >> /home/ec2-user/ChecksAndBalances/deploy.log 
 npm install >> /home/ec2-user/ChecksAndBalances/deploy.log
