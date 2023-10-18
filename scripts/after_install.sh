5546e605b82c01cfb1a269188a3e5cf14e4c947a
Nikpie7 <nikolai.w.c@gmail.com>
2023-10-18 17:03:07 -0400

    New Deployment v37

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 # Navigate to the project directory
-# cd /home/ec2-user/ChecksAndBalances/WebApp
-# Install project dependencies
-# npm install
-# npm install express --save
-# npm install body-parser
-# npm install mongodb
-# npm install cors
+
 
 cd /home/ec2-user/ChecksAndBalances/WebApp/frontend
 
@@ -16,3 +10,12 @@ npm ci
 npm install -g create-react-app
 npm install -g nodemon
 npm install react-router-dom
+
+
+cd /home/ec2-user/ChecksAndBalances/WebApp
+
+npm ci
+npm install express --save
+npm install body-parser
+npm install mongodb
+npm install cors
--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -5,3 +5,8 @@ cd /home/ec2-user/ChecksAndBalances/WebApp/frontend
 npm run build >> /home/ec2-user/ChecksAndBalances/deploy_2.log 
 
 pm2 start npm --name "my-app" -- start >> /home/ec2-user/ChecksAndBalances/deploy_2.log 
+
+cd /home/ec2-user/ChecksAndBalances/WebApp
+npm run build >> /home/ec2-user/ChecksAndBalances/deploy_2.log 
+
+pm2 start npm --name "my-backend" -- start >> /home/ec2-user/ChecksAndBalances/deploy_2.log 
