5c36467436353d7adfe4ea628e3cca4a6b241737
Nikpie7 <nikolai.w.c@gmail.com>
2023-10-18 15:51:01 -0400

    New Deployment v33

--- WebApp/frontend/src/components/PageTitle.js
+++ WebApp/frontend/src/components/PageTitle.js
@@ -3,7 +3,7 @@ import React from 'react';
 function PageTitle()
 {
    return(
-     <h1 id="title">COP 4331 MERN Stack Demo - MODIFIED FROM DEPLOYMENT!</h1>
+     <h1 id="title">COP 4331 MERN Stack Demo - Deployed from AWS!!!</h1>
    );
 };
 
