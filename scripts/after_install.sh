c6113b00d0489ea8a0738f05b21e6d91ad8c683b
Nikpie7 <nikolai.w.c@gmail.com>
2023-10-18 15:42:27 -0400

    New Deployment v32

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -13,7 +13,3 @@ npm install
 npm install -g create-react-app
 npm install -g nodemon
 npm install react-router-dom
-npm install pm2 -g
-ln -s "$(which node)" /sbin/node
-ln -s "$(which npm)" /sbin/npm
-ln -s "$(which pm2)" /sbin/pm2
