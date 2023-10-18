5b66b3511a6747a2f453a367d7e9e10812a73f5e
Nikpie7 <nikolai.w.c@gmail.com>
2023-10-18 00:57:42 -0400

    New Deployment v11

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+
+# Define the project directory
+PROJECT_DIR="/home/ec2-user/ChecksAndBalances/WebApp/frontend"
+
+# Remove the existing node_modules directory
+rm -rf "$PROJECT_DIR/node_modules"
+
+# Optionally, remove other generated files or directories
+rm -rf "$PROJECT_DIR/dist"
+rm -rf "$PROJECT_DIR/build"
+
+# If using a build tool like webpack, you might also have a clean command
+# npm run clean
+
