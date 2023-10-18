53b6ad1eac3329a6f29bb82ee1475b97f6cde3cc
Nikpie7 <nikolai.w.c@gmail.com>
2023-10-17 23:27:44 -0400

    New deployment v7

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-echo 'run after_install.sh: ' >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
+echo 'run after_install.sh: ' >> /home/ec2-user/ChecksAndBalances/deploy.log
 
-echo 'cd /home/ec2-user/nodejs-server-cicd' >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
-cd /home/ec2-user/nodejs-aws-codedeploy-pipeline >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
+echo 'cd /home/ec2-user/nodejs-server-cicd' >> /home/ec2-user/ChecksAndBalances/deploy.log
+cd /home/ec2-user/ChecksAndBalances >> /home/ec2-user/ChecksAndBalances/deploy.log
 
-echo 'npm install' >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log 
-npm install >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
+echo 'npm install' >> /home/ec2-user/ChecksAndBalances/deploy.log 
+npm install >> /home/ec2-user/ChecksAndBalances/deploy.log
--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo 'run application_start.sh: ' >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
+echo 'run application_start.sh: ' >> /home/ec2-user/ChecksAndBalances/deploy.log
 
-echo 'pm2 restart nodejs-express-app' >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
-pm2 restart all >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
+echo 'pm2 restart nodejs-express-app' >> /home/ec2-user/ChecksAndBalances/deploy.log
+pm2 restart all >> /home/ec2-user/ChecksAndBalances/deploy.log
