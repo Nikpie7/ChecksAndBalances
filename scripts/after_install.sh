ab800398899c3d62b9a8d81e6bc6ee5216c544ef
Nikpie7 <nikolai.w.c@gmail.com>
2023-10-18 14:02:57 -0400

    New Deployment v21

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -7,9 +7,12 @@ npm install express --save
 npm install body-parser
 npm install mongodb
 npm install cors
+npm install -g create-react-app
+npm install -g nodemon
 
 cd /frontend
 npm install
+npm 
 npm install react-router-dom
 # Build the React app
 npm run build
