495e51aa9b3e6e4793b5d28256ce8b81fc8574d7
Nikpie7 <nikolai.w.c@gmail.com>
2023-10-18 22:19:48 -0400

    New Deployment v38

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -6,16 +6,9 @@ cd /home/ec2-user/ChecksAndBalances/WebApp/frontend
 
 pm2 kill
 
-npm ci
-npm install -g create-react-app
-npm install -g nodemon
-npm install react-router-dom
+npm install
 
 
 cd /home/ec2-user/ChecksAndBalances/WebApp
 
-npm ci
-npm install express --save
-npm install body-parser
-npm install mongodb
-npm install cors
+npm install
--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 # Navigate to the project directory
-cd /home/ec2-user/ChecksAndBalances/WebApp/frontend
-# Start the Node.js server
-npm run build >> /home/ec2-user/ChecksAndBalances/deploy_2.log 
-
-pm2 start npm --name "my-app" -- start >> /home/ec2-user/ChecksAndBalances/deploy_2.log 
+# cd /home/ec2-user/ChecksAndBalances/WebApp/frontend
+# pm2 start npm --name "my-app" -- start >> /home/ec2-user/ChecksAndBalances/deploy_2.log 
 
 cd /home/ec2-user/ChecksAndBalances/WebApp
-npm run build >> /home/ec2-user/ChecksAndBalances/deploy_2.log 
-
 pm2 start npm --name "my-backend" -- start >> /home/ec2-user/ChecksAndBalances/deploy_2.log 
