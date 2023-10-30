d5daa7b381fed326fa5542a42eb6653d55e94e8b
TimKova <72708781+TimKova@users.noreply.github.com>
2023-10-29 22:31:29 -0400

    Update server.js
    Hello World!

--- WebApp/server.js
+++ WebApp/server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
 const PORT = process.env.PORT || 5001;
-
+// Hello World!
 const app = express();
 var cardList =
 [
