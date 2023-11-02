46a5e3d9c611520289d971d9fcba93dcf8c23a4e
Trump7 <ma332651@ucf.edu>
2023-11-01 21:52:26 -0400

    basics

---
+++ MobileApp/App.js
@@ -0,0 +1,31 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  useColorScheme,
+} from 'react-native';
+
+function App() {
+
+  return (
+    <SafeAreaView style={styles.root}>
+      
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+  }
+});
+
+export default App;
