a04515ff97a9b037f4f6b51932728ac7699324c5
KobeConomon <kobeconomon@knights.ucf.edu>
2023-11-02 01:00:28 -0400

    edited sign up screen

--- MobileApp/src/screens/SignUpScreen/SignUpScreen.js
+++ MobileApp/src/screens/SignUpScreen/SignUpScreen.js
@@ -1,28 +1,53 @@
-import React, {useState} from 'react';
-import {
-    View,
-    Text,
-    Image,
-    StyleSheet,
-    useWindowDimensions,
-    ScrollView,
-} from 'react-native';
+import React, { useState } from 'react'
+import {View, Text, StyleSheet, ScrollView} from 'react-native';
+import CustomInput from '../../components/CustomInput';
+import CustomButton from '../../components/CustomButton';
+
+const onLogInPressed = () => {
+    console.warn("Log in");
+}
+
+const onSignUpPressed = () => {
+    console.warn("Sign Up");
+}
 
 const SignUpScreen = () => {
     const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [passwordRepeat, setPasswordRepeat] = useState('');
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [streetAddress, setStreetAddress] = useState('');
+    const [zipCode, setZipCode] = useState('');
 
-    const {height} = useWindowDimensions();
+    return(
+        <View style={styles.root}>
+            <Image source={Logo} style={[styles.logo, {height: height * 0.3}]} resizeMode="contain"/>
+        
+            <CustomInput placeholder="Username" value={username} setValue={setUsername}/>
+            <CustomInput placeholder="Password" value={password} setValue={setPassword} secure={true}/>
+            <CustomInput placeholder="Confirm Password" value={passwordRepeat} setValue={setPasswordRepeat} secure={true}/>
+            <CustomInput placeholder="Email" value={email} setValue={setEmail}/>
+            <CustomInput placeholder="Your First Name" value={firstName} setValue={setFirstName}/>
+            <CustomInput placeholder="Your Last Name" value={lastName} setValue={setLastName}/>
+            <CustomInput placeholder="123 Main Street, Orlando, FL" value={streetAddress} setValue={setStreetAddress}/>
+            <CustomInput placeholder="12345" value={zipCode} setValue={setZipCode}/>
+            <CustomButton text="Register" onPress={onSignUpPressed} type="primary"/>
+        </View>
+    );
+};
 
-    const onSignInPressed = () => {
-        console.warn('Sign in');
-    }
+const styles = StyleSheet.create({
+    root:{
+        alignItems: 'center',
+        padding: 20,
+    },
+    logo: {
+        width: '70%',
+        maxWidth: 300,
+        maxHeight: 200,
+    },
+});
 
-    const onForgotPasswordPressed = () => {
-        console.warn('onForgotPasswordPressed');
-    }
-
-    const onSignInFacebook = () => {
-        console.warn('onSignInFacebook');
-    }
-}
+export default SignUpScreen;
