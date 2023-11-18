d0902bb422ce471b24409e5d0dfe5b0b357c2e23
KobeConomon <kobeconomon@knights.ucf.edu>
2023-11-17 20:56:51 -0500

    added dashboard screen

--- MobileApp/src/client/screens/DashboardScreen/DashboardScreen.js
+++ MobileApp/src/client/screens/DashboardScreen/DashboardScreen.js
@@ -1,6 +1,6 @@
 /* eslint-disable prettier/prettier */
 import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, TextInput, ScrollView, Image } from 'react-native';
+import { View, Text, TouchableOpacity, TextInput, ScrollView, Image, StyleSheet } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import  Modal  from 'react-native-modal';
@@ -41,6 +41,11 @@ const DashboardScreen = () => {
     setHamburgerModalVisible(!isHamburgerModalVisible);
   };
 
+  const handleTabChange = (tab) => {
+    // Add logic to update the feed based on the selected tab
+    console.log('Switch to tab:', tab);
+  };
+
   // ... rest of the code ...
 
   return (
@@ -54,8 +59,8 @@ const DashboardScreen = () => {
             {<Image source={require('../../components/ProfileImage/ProfileImage.png')} style={{ width: 30, height: 30 }} />}
           </TouchableOpacity>
 
-          <Text style= {{fontWeight: 500, flex: 1, fontSize: 20}}>
-            Hello, *insert user*
+          <Text style= {{fontWeight: 500, flex: 1, fontSize: 16}}>
+            Hello, *insert user later*
           </Text>
 
           <TextInput
@@ -92,7 +97,53 @@ const DashboardScreen = () => {
           </View>
         </Modal>
 
-        {/* ... rest of the code ... */}
+        {/* Tab Bar */}
+        <View style={{ flexDirection: 'row', justifyContent: 'space-around', backgroundColor: '#eee', paddingVertical: 8 }}>
+          {/* Tabs */}
+          <TouchableOpacity onPress={() => handleTabChange('Interests')}>
+            <Text>Interests</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress={() => handleTabChange('Votes')}>
+            <Text>Votes</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress={() => handleTabChange('All Bills')}>
+            <Text>All Bills</Text>
+          </TouchableOpacity>
+        </View>
+
+        {/* Feed Section */}
+        <ScrollView style={{ flex: 1, paddingHorizontal: 16 }}>
+          {/* Add your feed items based on the selected tab and user settings */}
+          <Text>Feed items go here and here Feed items go here and here Feed items go here and here Feed items go here and here
+          Feed items go here and here Feed items go here and here Feed items go here and here Feed items go here and here
+          Feed items go here and here Feed items go here and here Feed items go here and here Feed items go here and here
+          Feed items go here and here Feed items go here and here Feed items go here and here Feed items go here and here
+          Feed items go here and here Feed items go here and here Feed items go here and here Feed items go here and here
+          Feed items go here and here Feed items go here and here Feed items go here and here Feed items go here and here
+          Feed items go here and here Feed items go here and here Feed items go here and here Feed items go here and here
+          Feed items go here and here Feed items go here and here Feed items go here and here Feed items go here and here
+          Feed items go here and here Feed items go here and here Feed items go here and here Feed items go here and here
+          Feed items go here and here Feed items go here and here Feed items go here and here Feed items go here and here
+          Feed items go here and here Feed items go here and here Feed items go here and here Feed items go here and here
+          Feed items go here and here Feed items go here and here Feed items go here and here Feed items go here and here
+          Feed items go here and here Feed items go here and here Feed items go here and here Feed items go here and here
+          Feed items go here and here Feed items go here and here Feed items go here and here Feed items go here and here
+          Feed items go here and here Feed items go here and here Feed items go here and here Feed items go here and here
+          Feed items go here and here Feed items go here and here Feed items go here and here Feed items go here and here
+          Feed items go here and here Feed items go here and here Feed items go here and here Feed items go here and here
+          Feed items go here and here Feed items go here and here Feed items go here and here Feed items go here and here
+          Feed items go here and here Feed items go here and here Feed items go here and here Feed items go here and here
+          Feed items go here and here Feed items go here and here Feed items go here and here Feed items go here and here
+          Feed items go here and here Feed items go here and here Feed items go here and here Feed items go here and here
+          Feed items go here and here Feed items go here and here Feed items go here and here Feed items go here and here
+          Feed items go here and here Feed items go here and here Feed items go here and here Feed items go here and here
+          Feed items go here and here Feed items go here and here Feed items go here and here Feed items go here and here
+          Feed items go here and here Feed items go here and here Feed items go here and here Feed items go here and here
+          Feed items go here and here Feed items go here and here Feed items go here and here Feed items go here and here
+          </Text>
+        </ScrollView>
       </SafeAreaView>
     </View>
   );
@@ -103,129 +154,24 @@ export default DashboardScreen;
 
 
 
-// Display profile icon, implement profile functionality
-// Display first/last name
+// Implement profile functionality
+// First/last name API call
 // Implement hamburger bar functionality
     // implement hamburger option 1 functionality
     // implement hamburger option 2 functionality
     // implement hamburger option 3 functionality
-// Search bar display
 // Search bar functionality
 
-// Interests tab display
+// Interests tab styling
 // interests tab functionality
     // Scroll, fetch and implement data
 
-// New tab display
+// New tab styling
 // New tab functionality
     // Scroll, fetch and implement data
 
-// All bills tab display
+// All bills tab styling
 // All bills tab functionality
     // Scroll, fetch and implement data
 
-// Scrollbar functionality
-
-
-// import React, { useState } from "react";
-// import { ScrollView, View, Text, StatusBar, SafeAreaView, StyleSheet, RCTImageView} from "react-native";
-// import { useNavigation } from '@react-navigation/native';
-// // import ProfileImage from '../../components/ProfileImage/ProfileImage.png';
-
-// const ProfileImage = require('../../components/ProfileImage/ProfileImage.png').default;
-// JSON.stringify(ProfileImage);
-
-// const DashboardScreen = () => {
-//   const [items, setItems] = useState([
-//     {
-//       title: "Item 1",
-//       content: "This is the content of item 1.",
-//       key: "item1",
-//     },
-//     {
-//       title: "Item 2",
-//       content: "This is the content of item 2.",
-//       key: "item2",
-//     },
-//     {
-//       title: "Item 3",
-//       content: "This is the content of item 3.",
-//       key: "item3",
-//     },
-//     {
-//         title: "Item 4",
-//         content: "This is the content of item 4.",
-//         key: "item4",
-//     },
-//     {
-//         title: "Item 5",
-//         content: "This is the content of item 5.",
-//         key: "item5",
-//     },
-//     {
-//         title: "Item 6",
-//         content: "This is the content of item 6.",
-//         key: "item6",
-//     },
-//     {
-//         title: "Item 7",
-//         content: "This is the content of item 7.",
-//         key: "item7",
-//     },
-//     {
-//         title: "Item 8",
-//         content: "This is the content of item 8.",
-//         key: "item8",
-//     },
-//     {
-//         title: "Item 9",
-//         content: "This is the content of item 9.",
-//         key: "item9",
-//     },
-//   ]);
-
-//   // const [scrollY, setScrollY] = useState(0);
-
-//   // scrollEnabled={true} onScroll={(event) => setScrollY(event.nativeEvent.contentOffset.y)}
-//   return (
-//     <SafeAreaView style={styles.container} scrollEnabled={true} onScroll={(event) => setScrollY(event.nativeEvent.contentOffset.y)}>
-//       <RCTImageView 
-//         style={styles.imageView}
-//         source={{ uri: ProfileImage}}
-//       />
-//       <ScrollView style={styles.ScrollView}>
-//         {items.map((item) => (
-//           <View key={item.id}>
-//             <Text style={styles.textTitle}>{item.title}</Text>
-//             <Text style={styles.text}>{item.content}</Text>
-//           </View>
-//         ))}
-//       </ScrollView>
-//     </SafeAreaView>
-//   );
-// };
-
-// const styles = StyleSheet.create({
-//     container: {
-//       flex: 1,
-//       paddingTop: StatusBar.currentHeight,
-//       justifyContent: 'center',
-//       alignItems: 'center',
-//     },
-//     scrollView: {
-//       backgroundColor: 'pink',
-//       marginHorizontal: 20,
-//     },
-//     textTitle: {
-//       fontSize: 50,
-//     },
-//     text: {
-//       fontSize: 30,
-//     },
-//     imageView: {
-//       width: 100,
-//       height: 100,
-//     },
-//   });
-
-// export default DashboardScreen;
+// Scrollbar styling
