da295b244e78c1b001059e29d58ae8deedf268b4
Zandroid505 <zakariaantifit@hotmail.com>
2023-11-20 19:52:11 -0500

    Finally getting related interests from API and updated gitignore to ignore vscode folder.

--- WebApp/frontend/src/components/LegislationTable.js
+++ WebApp/frontend/src/components/LegislationTable.js
@@ -1,7 +1,7 @@
 import { render } from "@testing-library/react";
 import { React, useState } from "react";
 
-import legislativeBills from "./legislationListTestData";
+import bills from "./legislationListTestData";
 import "./LegislationTable.css";
 
 import dashboardService from "../utils/dashboardService.js";
@@ -19,62 +19,97 @@ const LegistlationTable = () => {
   // Hold bills.
   const [billList, setBillList] = useState([]);
 
-  const [billNum, setbillNum] = useState("");
+  // const [billNum, setbillNum] = useState("");
 
   // Get user's interests.
-  let interests = dashboardService.getReadInterests(USER_ID);
+  dashboardService.getReadInterests(USER_ID).then((interestsData) => {
+    // Hold interests data.
+    let interests = interestsData.Interests;
 
-  console.log(interests.Interests)
+    // Iterate through interests.
+    for (let i = 0; i < interests.length; i++) {
+      if (interests[i].value === true) {
+        // Get bill #s for current interest.
+        dashboardService
+          .getGetBillsByInterests(interests[i].InterestName)
+          .then((billNumbers) => {
+            // Iterate through all bills related to interest.
+            for (const billNumber in billNumbers) {
+              
+              // Get bill info for house and senate.
+              for (const billType in ["hr", "s"]) {
+                // Get bill titles.
+                let billTitles = dashboardService.getGetBillTitles(
+                  CONGRESS_NUM,
+                  billType,
+                  billNumber
+                );
 
-  // Get bill #s from list of interests.
-  // for (interests)
-  // if interest is true
-  // Get bill #s for interest
-  // for (bill #s)
-  // Get title.
-  // Get Committee(s).
-  // Get short description.
+                // Get Bill summaries.
+                let billSummary = dashboardService.getGetBillSummaries(
+                  CONGRESS_NUM,
+                  billType,
+                  billNumber
+                );
 
-  // Get bill title.
+                // Get bill committees.
+                let billcommittees = dashboardService.getGetBillCommittees(
+                  CONGRESS_NUM,
+                  billType,
+                  billNumber
+                );
 
-  // Get Committees attached to bill.
-  // Get bill's short description.
+                // Add bill info to list.
+                setBillList((billList) => [
+                  ...billList,
+                  {
+                    title: billTitles.titles[0].title,
+                    summary: billSummary.summaries[0].text,
+                    committees: billcommittees.committees[0].name,
+                  },
+                ]);
+              }
+            }
+          });
+      }
+    }
+  });
 
   return (
     <div>
       {/* LegistlationList */}
-      <LegistlationList data={legislativeBills} />
+      <LegistlationList bills={billList} />
     </div>
   );
 };
 
 const LegistlationList = (props) => {
-  let data = props.data;
-  const listRows = data.map(data => <h1>{data.billName}</h1>, <h2>{data.committee}</h2>)
+  let bills = props.bills;
 
   // Render the row for each piece of legislation.
-  const renderLegislationRow = (legislationObj, index) => {
+  const renderLegislationRow = (bill) => {
     return (
       <div
+        key={bill.title}
         id="legislationList-row"
-        class="grid grid-flow-row shadow-2xl p-4 m-8 rounded-lg hover:scale-105 duration-300"
+        className="grid grid-flow-row shadow-2xl p-4 m-8 rounded-lg hover:scale-105 duration-300"
       >
-        <li key={index}>
+        <li>
           {/* Mobile = Bill name + short description
               Web = Bill Name + short description + committee*/}
-          <h1>{legislationObj.billName}</h1>
-          <h2>{legislationObj.committee}</h2>
+          <h1>{bill.title}</h1>
+          <h2>{bill.committee}</h2>
         </li>
       </div>
     );
   };
 
   return (
-    <div class="grid justify-center md:justify-normal">
+    <div className="grid justify-center md:justify-normal">
       <ul>
-        {data.map((legislationObj, index) =>
+        {bills.map((bill) =>
           // Create rows.
-          renderLegislationRow(legislationObj, index)
+          renderLegislationRow(bill)
         )}
       </ul>
     </div>
--- WebApp/frontend/src/utils/dashboardService.js
+++ WebApp/frontend/src/utils/dashboardService.js
@@ -1,41 +1,99 @@
-import axios from 'axios';
-const baseUrl = process.env.NODE_ENV === 'development'
-  ? 'http://localhost:5001'
-  : 'https://checksnbalances.us';
+import axios from "axios";
+const baseUrl =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:5001"
+    : "https://checksnbalances.us";
 
 const getReadInterests = (request) => {
-  return axios.get(`${baseUrl}/api/readInterests`, request)
-    .then(response => {
+  return axios
+    .get(`${baseUrl}/api/readInterests`, {
+      params: {
+        userId: request.userId,
+      },
+    })
+    .then((response) => {
       console.log(response);
       return response.data;
     })
-    .catch(error => {
+    .catch((error) => {
       console.log(error);
     });
 };
 
 const getGetBillsByInterests = (request) => {
-  return axios.get(`${baseUrl}/api/getBillsByInterest`, request)
-    .then(response => {
+  return axios
+    .get(`${baseUrl}/api/getBillsByInterest`, {
+      params: {
+        interest: request.interestName,
+      },
+    })
+    .then((response) => {
       console.log(response);
       return response.data;
     })
-    .catch(error => {
+    .catch((error) => {
       console.log(error);
     });
 };
 
 const getGetBillTitles = (request) => {
-    return axios.get(`${baseUrl}/api/getBillTitles`, request)
-      .then(response => {
-        console.log(response);
-        return response.data;
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  };
+  return axios
+    .get(`${baseUrl}/api/getBillTitles`, {
+      params: {
+        congress: request.congress,
+        billType: request.billType,
+        billNumber: request.billNumber,
+      },
+    })
+    .then((response) => {
+      console.log(response);
+      return response.data;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
 
+const getGetBillCommittees = (request) => {
+  return axios
+    .get(`${baseUrl}/api/getBillCommittees`, {
+      params: {
+        congress: request.congress,
+        billType: request.billType,
+        billNumber: request.billNumber,
+      },
+    })
+    .then((response) => {
+      console.log(response);
+      return response.data;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
 
+const getGetBillSummaries = (request) => {
+  return axios
+    .get(`${baseUrl}/api/getBillSummaries`, {
+      params: {
+        congress: request.congress,
+        billType: request.billType,
+        billNumber: request.billNumber,
+      },
+    })
+    .then((response) => {
+      console.log(response);
+      return response.data;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
 
-export default { getReadInterests, getGetBillsByInterests};
+export default {
+  getReadInterests,
+  getGetBillsByInterests,
+  getGetBillTitles,
+  getGetBillCommittees,
+  getGetBillSummaries,
+};
