fc4c5643d3cc3d6e040d2099e67f15e0aac823f4
Zandroid505 <zakariaantifit@hotmail.com>
2023-11-26 19:43:08 -0500

    Bills sorted by date in descending order (Most recent appearing first).

--- WebApp/frontend/src/components/LegislationTable.js
+++ WebApp/frontend/src/components/LegislationTable.js
@@ -2,12 +2,13 @@ import { render } from "@testing-library/react";
 import { React, useState } from "react";
 import { useQuery } from "react-query";
 
+import interestsCategories from "./interestsCategories.json";
 import "./LegislationTable.css";
 
 import dashboardService from "../utils/dashboardService.js";
 
 // ONLY FOR TESTING!!!!
-// TODO: REOVE THIS
+// TODO: REMOVE THIS
 const USER_ID = { userId: "65580d043bdf8a775970f892" };
 
 // Define the current Congress.
@@ -48,6 +49,40 @@ const LegistlationTable = () => {
 
 const LegistlationList = (props) => {
   const userInterests = props.userInterests;
+  let billListTemp = [];
+
+  // Get bills for user's interests.
+  const {
+    data: bills,
+    isLoading,
+    isError,
+  } = useQuery(["billsData", interestsCategories], () =>
+    Promise.all(
+      userInterests.map(async (interest) =>
+        dashboardService.postSearchBillsByInterest({ Interest: interest })
+      )
+    )
+  );
+
+  //TODO: Add custom animations.
+  if (isLoading) return <p>Loading...</p>;
+  if (isError) return <p>Error...</p>;
+
+  // Iterate through each interest category.
+  for (let i = 0; i < bills.length; i++) {
+    // Iterate through all bills for current interest.
+    for (let j = 0; j < bills[i].response.length; j++) {
+      billListTemp.push(bills[i].response[j]);
+    }
+  }
+
+  // Sort all the bills by date.
+  billListTemp.sort(
+    (a, b) => new Date(a.LastUpdated) - new Date(b.LastUpdated)
+  );
+
+  // Reverse so it's in descending order.
+  billListTemp.reverse();
 
   return (
     <div className="grid justify-normal">
@@ -57,8 +92,8 @@ const LegistlationList = (props) => {
             {/* Mobile = Bill name + short description
                 Web = Bill Name + short description + committee*/}
             {/* Iterate through each interest. */}
-            {userInterests.map((interest) => (
-              <BillList key={interest} Interest={interest} />
+            {billListTemp.map((bill) => (
+              <Bill key={bill._id} currBill={bill} />
             ))}
           </li>
         }
@@ -67,50 +102,20 @@ const LegistlationList = (props) => {
   );
 };
 
-const BillList = (props) => {
-  const currInterest = props.Interest;
-
-  // Get bills for current interest.
-  const {
-    data: bills,
-    isLoading,
-    isError,
-  } = useQuery(["billsData", { Interest: currInterest }], () =>
-    dashboardService.postSearchBillsByInterest({ Interest: currInterest })
-  );
-
-  //TODO: Add custom animations.
-  if (isLoading) return <p>Loading...</p>;
-  if (isError) return <p>Error...</p>;
-
-  // Hold bills temporarily.
-  let billList = bills.response;
-
-  return (
-    <div className="">
-      {/* Iterate through each bill. */}
-      {billList.map((bill) => (
-        <div
-          key={bill._id}
-          id="legislationList-row"
-          className="shadow-2xl p-4 m-8 rounded-lg hover:scale-105 duration-300"
-        >
-          <Bill currBill={bill} />
-        </div>
-      ))}
-    </div>
-  );
-};
-
 const Bill = (props) => {
   const bill = props.currBill;
 
   return (
-    <div>
+    <div
+      id="legislationList-row"
+      className="shadow-2xl p-4 m-8 rounded-lg hover:scale-105 duration-300"
+    >
       <h1 className="md:text-lg line-clamp-2 xl:line-clamp-1">
         <b>{bill.Title}</b>
       </h1>
-      <h2>{bill.BillType.toUpperCase()}.{bill.BillNumber}</h2>
+      <h2>
+        {bill.BillType.toUpperCase()}.{bill.BillNumber}
+      </h2>
     </div>
   );
 };
