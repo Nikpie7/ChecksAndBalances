f0efcb547a80e160cd3a2cec5f55e59d4ea01cda
Trump7 <ma332651@ucf.edu>
2023-11-28 16:25:31 -0500

    Changed verification methods, added success modal

--- MobileApp/src/client/screens/SignUpScreen/SignUpScreen.js
+++ MobileApp/src/client/screens/SignUpScreen/SignUpScreen.js
@@ -7,7 +7,7 @@ import { useNavigation } from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/Ionicons';
 import FloatingLabelInput from '../../components/FloatingLabelInput/FloatingLabelInput';
 import PasswordComplex from '../../components/PasswordComplex/PasswordComplex';
-
+import Modal from 'react-native-modal';
 
 const SignUpScreen = () => {
     const [username, setUsername] = useState('');
@@ -19,13 +19,18 @@ const SignUpScreen = () => {
     const [streetAddress, setStreetAddress] = useState('');
     const [zipCode, setZipCode] = useState('');
     const [emptyInputs, setEmptyInputs] = useState([]);
-    const [error, setError] = useState('');
+    const [errors, setErrors] = useState([]);
+    const [validEmail, setValidEmail] = useState(true);
+    const [validPass, setValidPass] = useState(true);
 
+    const [isSuccessModalVisible, setIsSuccessModalVisible] = useState(false);
     const navigation = useNavigation();
 
     
 
     const onSignUpPressed = () => {
+        setValidEmail(true);
+        setValidPass(true);
         //Validation to see if all fields are filled
         const inputs = [username, password, passwordRepeat, email, firstName, lastName, streetAddress, zipCode];
         const emptyInputIndex = inputs.reduce((acc, input, index) => {
@@ -35,45 +40,54 @@ const SignUpScreen = () => {
             return acc;
         }, []);
 
+        const validationErrors = [];
+
         if(emptyInputIndex.length > 0){
             const inputNames = ['Username', 'Password', 'Confirm Password', 'Email', 'First Name', 'Last Name', 'Street Address', 'Zip Code'];
-            setEmptyInputs(emptyInputIndex);
             const emptyFields = emptyInputIndex.map(index => inputNames[index]);
-            setError(`Please Fill out: ${emptyFields.join(', ')}`);
-            console.log('All fields are not filled out');
-            return;
+            validationErrors.push(`Please Fill out: ${emptyFields.join(', ')}`);
         }
+
+        // Regular expression to check for '@' in the email
+        const isValidEmailFormat = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+
         //Validation for password matching
         if(password !== passwordRepeat){
-            setEmptyInputs([1, 2]);
-            setError("Error: Passwords do not match");
-            return;
+            setValidPass(false);
+            validationErrors.push("Passwords do not match");
         }
         //Validation for password length
         if(password.length < 8){
-            setEmptyInputs([1, 2]);
-            setError("Error: Password must be greater than 8 characters");
-            return;
+            setValidPass(false);
+            validationErrors.push("Password must be greater than 8 characters");
         }
         //Validation for password number
         if(!(/\d/.test(password))){
-            setEmptyInputs([1, 2]);
-            setError("Error: Password must contain numbers");
-            return;
+            setValidPass(false);
+            validationErrors.push("Password must contain numbers");
         }
         //Validation for password Upper
         if(!(/[A-Z]/.test(password))){
-            setEmptyInputs([1, 2]);
-            setError("Error: Password must contain at least one uppercase letter");
-            return;
+            setValidPass(false);
+            validationErrors.push("Password must contain at least one uppercase letter");
         }
         //Validation for password Special
         if(!(/[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]/.test(password))){
-            setEmptyInputs([1, 2]);
-            setError("Error: Password must contain at least one special character");
-            return;
+            setValidPass(false);
+            validationErrors.push("Password must contain at least one special character");
         }
 
+        if (!isValidEmailFormat) {
+            setValidEmail(false);
+            validationErrors.push("Please enter a valid email address");
+        }
+
+        setErrors(validationErrors);
+        setEmptyInputs(emptyInputIndex);
+
+        if(validationErrors.length > 0){
+            return;
+        }
         //Passed Validation
         //gather up all fields
         bodyVariable = JSON.stringify({
@@ -101,13 +115,11 @@ const SignUpScreen = () => {
             console.log(data);
             if(data.error == ""){
                 //redirect to log in screen
+                setIsSuccessModalVisible(true);
                 console.warn("Registration Complete! Check Email for Varification!");
-                navigation.navigate('SignIn');
             }
             else{
-                console.log("Email was not imputed correctly");
-                setError("Error: Email has incorect format\n(example@domain.com)");
-                setEmptyInputs([3])
+                console.log("Error Processing email verifciation");
             }
         })
         .catch(error => {
@@ -129,17 +141,22 @@ const SignUpScreen = () => {
                 </View>
                 <View style={styles.inputContainer}>
                     <View style={styles.whiteBox}>
-                        {error && (
-                            <View style={styles.errorBanner}>
-                                <Text style={styles.errorText}>{error}</Text>
-                            </View>
+                    {errors.length > 0 && (
+                        <View style={styles.errorBanner}>
+                            <Text style={styles.errorText}>Error:</Text>
+                            {errors.map((errorMessage, index) => (
+                                <Text key={index} style={styles.errorText}>
+                                    {errorMessage}
+                                </Text>
+                            ))}
+                        </View>
                         )}
 
                         <FloatingLabelInput style={[styles.input, emptyInputs.includes(0) && styles.inputError,]} label="Username" value={username} onChangeText={setUsername}/>
-                        <FloatingLabelInput style={[styles.input, emptyInputs.includes(1) && styles.inputError,]} label="Password" value={password} onChangeText={setPassword} secureTextEntry/>
+                        <FloatingLabelInput style={[styles.input, emptyInputs.includes(1) && styles.inputError, !validPass && styles.inputError,]} label="Password" value={password} onChangeText={setPassword} secureTextEntry/>
                         <PasswordComplex password={password}/>
-                        <FloatingLabelInput style={[styles.input, emptyInputs.includes(2) && styles.inputError,]} label="Confirm Password" value={passwordRepeat} onChangeText={setPasswordRepeat} secureTextEntry/>
-                        <FloatingLabelInput style={[styles.input, emptyInputs.includes(3) && styles.inputError,]} label="Email" value={email} onChangeText={setEmail}/>
+                        <FloatingLabelInput style={[styles.input, emptyInputs.includes(2) && styles.inputError, !validPass && styles.inputError,]} label="Confirm Password" value={passwordRepeat} onChangeText={setPasswordRepeat} secureTextEntry/>
+                        <FloatingLabelInput style={[styles.input, emptyInputs.includes(3) && styles.inputError, !validEmail && styles.inputError,]} label="Email" value={email} onChangeText={setEmail}/>
                         <FloatingLabelInput style={[styles.input, emptyInputs.includes(4) && styles.inputError,]} label="First Name" value={firstName} onChangeText={setFirstName}/>
                         <FloatingLabelInput style={[styles.input, emptyInputs.includes(5) && styles.inputError,]} label="Last Name" value={lastName} onChangeText={setLastName}/>
                         <FloatingLabelInput style={[styles.input, emptyInputs.includes(6) && styles.inputError,]} label="Street Address, City, State" value={streetAddress} onChangeText={setStreetAddress}/>
@@ -151,6 +168,26 @@ const SignUpScreen = () => {
                     </View>
                 </View>
             </ScrollView>
+            <Modal isVisible={isSuccessModalVisible} style={styles.modal}>
+                <View style={styles.modalContainer}>
+                    <View style={[styles.whiteBox, styles.modalContent]}>
+                        <Text style={styles.modalText}>
+                            Registration Complete,{"\n"} Check Email for Verification!
+                        </Text>
+                        <TouchableOpacity
+                            style={[styles.button, styles.modalButton]}
+                            onPress={() => {
+                                setIsSuccessModalVisible(false);
+                                navigation.navigate('SignIn');
+                            }}
+                        >
+                            <Text style={[styles.buttonText, styles.modalButtonText]}>
+                                Return to Login
+                            </Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </Modal>
         </View>
     );
 };
@@ -227,6 +264,42 @@ const styles = StyleSheet.create({
     errorText: {
         color: 'white',
         textAlign: 'center',
+    },
+    modal: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: 0,
+    },
+    modalContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    modalContent: {
+        width: '80%',
+        backgroundColor: 'white',
+        padding: 20,
+        borderRadius: 10,
+        alignItems: 'center',
+    },
+    modalText: {
+        fontSize: 18,
+        marginBottom: 20,
+        textAlign: 'center',
+    },
+    modalButton: {
+        backgroundColor: '#04ACD9DD',
+        padding: 15,
+        borderRadius: 10,
+    },
+    buttonText: {
+        color: 'white',
+        fontWeight: 'bold',
+        textAlign: 'center',
+        fontSize: 18,
+    },
+    modalButtonText: {
+        fontSize: 22,
     },
 });
 
