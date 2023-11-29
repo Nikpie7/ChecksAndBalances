a5d014957a81d267523061829d98ee299e7cfa53
KobeConomon <kobeconomon@knights.ucf.edu>
2023-11-29 01:46:47 -0500

    added UserContext

---
+++ MobileApp/src/client/components/UserContext/UserContext.js
@@ -0,0 +1,22 @@
+/* eslint-disable prettier/prettier */
+import React, { createContext, useState } from 'react';
+
+export const UserContext = createContext();
+
+export const UserProvider = ({ children }) => {
+  const [user, setUser] = useState({
+    firstName: '',
+    lastName: '',
+    userID: '',
+  });
+
+  const updateUser = (userData) => {
+    setUser(userData);
+  };
+
+  return (
+    <UserContext.Provider value={{ user, updateUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
